1921b44c61f888d83c11de2dbb48be34df1c648a
Caio Cesar <caiocsmeneghelli@outlook.com>
2023-09-09 18:28:33 -0300

    Adicionando mais DataAnnotation

--- ProEventos.Application/Dtos/EventoDto.cs
+++ ProEventos.Application/Dtos/EventoDto.cs
@@ -13,10 +13,19 @@ namespace ProEventos.Application.Dtos
         //MaxLength(50, ErrorMessage = "{0} deve possuir no máximo 50 caracteres.")]
         StringLength(50, MinimumLength = 3, ErrorMessage = "O campo {0} deve possuir o máximo de 50 caracteres e o mínimo de 3.")]
         public string Tema { get; set; }
+
+        [Display(Name = "Qtd de pessoas")]
+        [Required (ErrorMessage ="O campo {0} é obrigatório.")]
+        [Range(1, 120000, ErrorMessage = "O campo {0} deve possuir no mínimo 1 e no máximo 120000.")]
         public int QtdPessoas { get; set; }
         public string ImagemURL { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [Phone(ErrorMessage = "O campo {0} esta inválido.")]
         public string Telefone { get; set; }
 
+        [Display(Name = "e-mail")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         [EmailAddress(ErrorMessage = "O campo {0} precisar ser válido.")]
         public string Email { get; set; }
         public IEnumerable<LoteDto>? Lotes { get; set; }
