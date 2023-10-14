f3706d45c90bb016b950c9de202781fb51eab3cf
Caio Cesar <caiocsmeneghelli@outlook.com>
2023-10-14 20:34:31 -0300

    Add migration e adicionando nullable para imagem url.

--- ProEventos.Domain/Entities/Evento.cs
+++ ProEventos.Domain/Entities/Evento.cs
@@ -7,7 +7,7 @@ namespace ProEventos.Domain.Entities
         public DateTime? DataEvento { get; set; }
         public string Tema { get; set; }
         public int QtdPessoas { get; set; }
-        public string ImagemURL { get; set; }
+        public string ?ImagemURL { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
         public IEnumerable<Lote>? Lotes { get; set; }
---
+++ ProEventos.Persistence/Migrations/20231014232559_Add nullable property from imgamUrl.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProEventos.Persistence.Migrations
+{
+    public partial class AddnullablepropertyfromimgamUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lotes_Eventos_EventoId",
+                table: "Lotes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RedesSociais_Eventos_EventoId",
+                table: "RedesSociais");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RedesSociais_Palestrantes_PalestranteId",
+                table: "RedesSociais");
+
+            migrationBuilder.RenameColumn(
+                name: "EventoId",
+                table: "Lotes",
+                newName: "EventoID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Lotes_EventoId",
+                table: "Lotes",
+                newName: "IX_Lotes_EventoID");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Lotes",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagemURL",
+                table: "Eventos",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lotes_Eventos_EventoID",
+                table: "Lotes",
+                column: "EventoID",
+                principalTable: "Eventos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RedesSociais_Eventos_EventoId",
+                table: "RedesSociais",
+                column: "EventoId",
+                principalTable: "Eventos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RedesSociais_Palestrantes_PalestranteId",
+                table: "RedesSociais",
+                column: "PalestranteId",
+                principalTable: "Palestrantes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lotes_Eventos_EventoID",
+                table: "Lotes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RedesSociais_Eventos_EventoId",
+                table: "RedesSociais");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RedesSociais_Palestrantes_PalestranteId",
+                table: "RedesSociais");
+
+            migrationBuilder.RenameColumn(
+                name: "EventoID",
+                table: "Lotes",
+                newName: "EventoId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Lotes_EventoID",
+                table: "Lotes",
+                newName: "IX_Lotes_EventoId");
+
+            migrationBuilder.UpdateData(
+                table: "Lotes",
+                keyColumn: "Nome",
+                keyValue: null,
+                column: "Nome",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Lotes",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Eventos",
+                keyColumn: "ImagemURL",
+                keyValue: null,
+                column: "ImagemURL",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagemURL",
+                table: "Eventos",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lotes_Eventos_EventoId",
+                table: "Lotes",
+                column: "EventoId",
+                principalTable: "Eventos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RedesSociais_Eventos_EventoId",
+                table: "RedesSociais",
+                column: "EventoId",
+                principalTable: "Eventos",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RedesSociais_Palestrantes_PalestranteId",
+                table: "RedesSociais",
+                column: "PalestranteId",
+                principalTable: "Palestrantes",
+                principalColumn: "Id");
+        }
+    }
+}
