5bb5756998a51b3e18d830be8b0a136f970848b6
Caio Cesar <caiocsmeneghelli@outlook.com>
2023-10-18 22:48:34 -0300

    Finalizando o user service.

---
+++ ProEventos.Application/Services/UserService.cs
@@ -0,0 +1,124 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ProEventos.Application.Dtos;
+using ProEventos.Application.Interfaces;
+using ProEventos.Domain.Identity;
+using ProEventos.Persistence.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProEventos.Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IMapper _mapper;
+        private readonly IUserRepository _userRepository;
+
+        public UserService(UserManager<User> userManager,
+                            SignInManager<User> signInManager,
+                            IMapper mapper,
+                            IUserRepository userRepository)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _mapper = mapper;
+            _userRepository = userRepository;
+        }
+        public async Task<SignInResult> CheckUserPasswordAsync(UserUpdateDto userUpdateDto, string password)
+        {
+            try
+            {
+                var user = await _userManager.Users
+                    .SingleOrDefaultAsync(u => u.UserName == userUpdateDto.UserName.ToLower());
+                return await _signInManager.CheckPasswordSignInAsync(user, password, false);
+
+            }catch(Exception ex)
+            {
+                throw new Exception($"Erro ao tentar verificar password. Erro: {ex.Message}");
+            }
+        }
+
+        public async Task<UserDto> CreateUserAsync(UserDto userDto)
+        {
+            try
+            {
+                var user = _mapper.Map<User>(userDto);
+                var result = await _userManager.CreateAsync(user, userDto.Password);
+
+                if(result.Succeeded)
+                {
+                    var userToReturn = _mapper.Map<UserDto>(user);
+                    return userToReturn;
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro ao criar usuário. Erro: {ex.Message}");
+            }
+        }
+
+        public async Task<UserUpdateDto> GetUserByUserNameAsync(string userName)
+        {
+            try
+            {
+                var user = await _userRepository.GetUserByUserNameAsync(userName);
+                if (user == null) return null;
+
+                var userUpdate = _mapper.Map<UserUpdateDto>(user);
+                return userUpdate;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro ao tentar buscar usuário. Erro: {ex.Message}");
+            }
+        }
+
+        public async Task<UserUpdateDto> UpdateUser(UserUpdateDto userUpdateDto)
+        {
+            try
+            {
+                var user = await _userRepository.GetUserByUserNameAsync(userUpdateDto.UserName.ToLower());
+                if (user == null) return null;
+
+                _mapper.Map(userUpdateDto, user);
+
+                var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+                var result = await _userManager.ResetPasswordAsync(user, token, userUpdateDto.Password);
+
+                _userRepository.Update<User>(user);
+
+                if(await _userRepository.SaveChangesAsync())
+                {
+                    var userRetorno = await _userRepository.GetUserByUserNameAsync(user.UserName.ToLower());
+                    return _mapper.Map<UserUpdateDto>(userRetorno);
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro ao tentar atualizar usuário. Erro: {ex.Message}");
+            }
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            try
+            {
+                return await _userManager.Users.AnyAsync(u => u.UserName == username.ToLower());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro ao tentar verificar se o usuário existe. Erro: {ex.Message}");
+            }
+        }
+    }
+}
