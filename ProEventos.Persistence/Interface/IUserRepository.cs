db945434233f3dd2a829a5584e40a03f2cb83e09
Caio Cesar <caiocsmeneghelli@outlook.com>
2023-10-18 21:17:27 -0300

    Class e interface de usuarios.

---
+++ ProEventos.Persistence/Interface/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProEventos.Domain.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProEventos.Persistence.Interface
+{
+    public interface IUserRepository : IProEventosRepository
+    {
+        Task<IEnumerable<User>> GetUsersAsync();
+        Task<User> GetUserByIdAsync(int id);
+        Task<User> GetUserByUserNameAsync(string userName);
+    }
+}
---
+++ ProEventos.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using ProEventos.Domain.Identity;
+using ProEventos.Persistence.Context;
+using ProEventos.Persistence.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProEventos.Persistence.Repositories
+{
+    public class UserRepository : ProEventosRepository, IUserRepository
+    {
+        private readonly ProEventosContext _context;
+        public UserRepository(ProEventosContext context) : base(context)
+        {
+            _context = context;
+        }
+        public Task<User> GetUserByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserByUserNameAsync(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
