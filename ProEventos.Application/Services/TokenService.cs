ff298111db2a84538b90e37c2058eb2ff82277f0
Caio Cesar <caiocsmeneghelli@outlook.com>
2023-10-28 18:35:52 -0300

    Adicionando token service.

---
+++ ProEventos.Application/Services/TokenService.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using ProEventos.Application.Dtos;
+using ProEventos.Application.Interfaces;
+using ProEventos.Domain.Identity;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ProEventos.Application.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly UserManager<User> _userManager;
+        private readonly IMapper _mapper;
+
+        public readonly SymmetricSecurityKey _key;
+
+        public TokenService(IConfiguration configuration, UserManager<User> userManager, IMapper mapper)
+        {
+            _configuration = configuration;
+            _userManager = userManager;
+            _mapper = mapper;
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["TokenKey"]));
+        }
+        public async Task<string> CreateToken(UserUpdateDto userUpdateDto)
+        {
+            User user = _mapper.Map<User>(userUpdateDto);
+            var claims = new List<Claim>() {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName)
+            };
+
+            var roles = _userManager.GetRolesAsync(user).Result;
+
+            claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescription = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescription);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
