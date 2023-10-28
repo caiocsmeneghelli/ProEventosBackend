f616211671a7fbc8f70148585fac52df41e46b21
Caio Cesar <caiocsmeneghelli@outlook.com>
2023-10-28 19:35:42 -0300

    Adicionando mais configuracoes dentro do Program.

--- ProEventos.Api/Program.cs
+++ ProEventos.Api/Program.cs
@@ -1,17 +1,36 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
 using ProEventos.Application.Interfaces;
 using ProEventos.Application.Services;
+using ProEventos.Domain.Identity;
 using ProEventos.Persistence.Context;
 using ProEventos.Persistence.Interface;
 using ProEventos.Persistence.Repositories;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddIdentityCore<User>(opt =>
+{
+    opt.Password.RequireDigit = false;
+    opt.Password.RequireNonAlphanumeric = false;
+    opt.Password.RequireLowercase = false;
+    opt.Password.RequireUppercase = false;
+    opt.Password.RequiredLength = 4;
+})
+    .AddRoles<Role>()
+    .AddRoleManager<RoleManager<Role>>()
+    .AddSignInManager<SignInManager<User>>()
+    .AddRoleValidator<RoleValidator<Role>>()
+    .AddEntityFrameworkStores<ProEventosContext>()
+    .AddDefaultTokenProviders();
+
 builder.Services.AddControllers()
-    .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = 
+    .AddJsonOptions(opt => opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()))
+    .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling =
         Newtonsoft.Json.ReferenceLoopHandling.Ignore
     );
 
@@ -36,9 +55,11 @@ builder.Services.AddScoped<ILoteRepository, LoteRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 
-builder.Services.AddCors(option => {
+builder.Services.AddCors(option =>
+{
     option.AddPolicy("MyAllowedOrigins",
-    policy => {
+    policy =>
+    {
         //policy.WithOrigins("http://localhost:4200")        
         policy.AllowAnyOrigin()
             .AllowAnyHeader()
