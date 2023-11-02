7a525711902b8db961b94e7853c25a16c6efdbe3
Caio Cesar <caiocsmeneghelli@outlook.com>
2023-11-02 12:30:02 -0300

    Criando usercontroller

---
+++ ProEventos.Api/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProEventos.Application.Interfaces;
+
+namespace ProEventos.Api.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly ITokenService _tokenService;
+
+        public UserController(IUserService userService, ITokenService tokenService)
+        {
+            _userService = userService;
+            _tokenService = tokenService;
+        }
+
+        [HttpGet]
+        [Route("GetUser")]
+        public async Task<IActionResult> GetUser()
+        {
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar recuperar Usuário. Erro: {ex.Message}");
+            }
+        }
+    }
+}
